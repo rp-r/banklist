79eddd62d118fb43417bdd81478697168dd1ec0f
mohsin Raza <3ggjs24razamo@folkuniversitetet.nu>
2026-02-09 14:00:48 +0100

    add more data

--- script.js
+++ script.js
@@ -118,12 +118,19 @@ const displaymovements = function (movements) {
 
 displaymovements(account1.movements);
 
+/** Add the values from the array and display the balance */
+
 const displayBalance = function (data) {
   const b = data.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${b} euro`;
+  labelBalance.textContent = `${b} EUR`;
 };
 
+
 displayBalance(account1.movements);
+
+
+
+
 
 const eurToUSD = 1.1;
 
@@ -161,3 +168,191 @@ console.log(balance);
 const ba = movements.reduce((acc, mov) => acc + mov, 0);
 
 console.log(`Balance ${ba}`);
+
+/* 158 Compute the user name */
+
+
+const user1="Steven Thomas Williams";
+
+const username=user1.toLowerCase().split(' ').map(
+  name=>name[0]
+).join('');
+
+/* Now create a function */
+
+const getusername=function(user)
+{
+const username=user.toLowerCase().split(' ').map(
+  name=>name[0]
+).join('');
+
+return username;
+}
+
+
+
+ const getsname=accounts.map(user=>getusername(user.owner))
+
+
+
+console.log(getsname);
+
+/** Task is to create username propert in for each object */
+
+
+const createsusename= function(accs)
+{
+accs.forEach( function(acc){
+acc.username=acc.owner
+.toLowerCase()
+.split(' ')
+.map(name=>name[0])
+.join('');
+
+});
+};
+
+createsusename(accounts);
+
+console.log(accounts);
+
+
+
+/** The Filter Method
+
+*/
+
+
+const newdeposit=movements.filter(mov=>mov>0);
+const newwithdrawal=movements.filter(mov=>mov>0);
+
+console.log(movements);
+console.log(newdeposit);
+console.log(withdrawal)
+
+/** 160 The reduce Method
+ * 
+ * REDUCE METHOD: TO SUM ALL  THE VALUES IN SINGLE 
+ * Working to sum the values in the  array 
+ * 
+ */
+
+ const newbalance=movements.reduce((acc,cur)=>acc+cur,0);
+ console.log(newbalance)
+
+
+ /*** GETTING THE MAXIMUM VALUED FROM THE ARRAY movements: [200, 450, -400, 3000, -650, -130, 70, 1300];*/
+const max=movements.reduce( (acc,mov)=> acc>mov?acc:mov,0)
+console.log(max)
+//
+
+
+/** CHALLENGE # 1
+ * /*
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 🙂
+*/
+
+
+
+
+
+ const checkDogs=function(dogsJulia,dogsKate)
+ {
+const newdogsJulia=dogsJulia.slice();
+newdogsJulia.splice(0,1);
+newdogsJulia.splice(-2);
+const dogs=newdogsJulia.concat(dogsKate)
+console.log(dogs);
+
+dogs.forEach( function(dog,i)
+{
+
+  if(dog<=3)
+  {
+    console.log(` Dog num:  ${i} is Puppy and ${dog} years old    `)
+  }
+  else
+  {
+console.log(`Dog num:  ${i}  is adult and ${dog} years old `)
+  }
+
+})
+
+ }
+
+ checkDogs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3])
+
+/** CHALLENEGE # 2/*
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 🙂
+*/
+
+const calcAverageHumanAge= function (dogs)
+{
+    const humAage=dogs
+    .map(dogAge=>dogAge<=2? 2*dogAge: 16 +  16 + dogAge * 4)
+    .filter(dogAge=>dogAge>=18)
+    .reduce((acc,coun,i,arr)=>acc+coun/arr.length,0)
+   
+
+
+
+    return humAage;
+  
+}
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]))
+
+
+/**CHANLLENEGE # 3// Coding Challenge #3
+
+/*
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 🙂
+*/
+ 
+
+
+/*** 162 The Magic of Chaning Methods  */
+
+//Pipeline
+
+const eurtousd1=1.1;
+
+const Totaldeposit=movements
+.filter(mov=>mov>0)
+.map(con=>con*eurtousd1)
+.reduce((acc,cur)=>acc+cur,0)
+
+console.log(Math.trunc(Totaldeposit));
